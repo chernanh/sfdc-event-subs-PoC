bbee72f8a3c23c28db570c5e88932d006eb14f29
Carlos H <carlos.hernandez@everis.com>
2017-07-05 09:09:33 +0000

    Working subscription

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if
 exports.DEBUG = true;
 exports.ENVIRONMENT = 'production';
 exports.CALLBACK_URL = 'http://localhost:3001';
-exports.TOPIC = '/event/Order_Response__e';
+exports.TOPIC = '/event/vrk99__Testing_Event__e';
 
-exports.CLIENT_ID = "3MVG9SemV5D80oBcjsq97hHAUFcvbzU68h8Qd3DoYvB4fHUxGviPd57zmDEzTAFEsbaQUGyY1OOxJ_ACNMv5s";
-exports.CLIENT_SECRET = "8410631505093561599";
-exports.USERNAME = "admin@acmesales.com";
-exports.PASSWORD = "test1234";
+exports.CLIENT_ID = "3MVG9Rd3qC6oMalU9PrzvzcdHLAI_C2zTggV1x8CIN1bnS_O_yP59fRnW7e5624YsHhTvxXU.fneLVL4tpNnz";
+exports.CLIENT_SECRET = "5837361346250273960";
+exports.USERNAME = "api@vrakmhare99.com";
+exports.PASSWORD = "Everis.072017obuOLbYkBRDlkwlXML8xF8l7";
